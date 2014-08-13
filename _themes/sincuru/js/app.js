6f81eca2039d004d0dc0bc1ff912f26b2abedfb6
Mario Vassilides <mario@serieseight.com>
2014-08-13 21:19:27 +0100

    add crazyegg

--- _themes/sincuru/js/app.js
+++ _themes/sincuru/js/app.js
@@ -26,3 +26,11 @@ __lc.license = 4838831;
     lc.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'cdn.livechatinc.com/tracking.js';
     var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(lc, s);
 })();
+
+
+
+setTimeout(function(){var a=document.createElement("script");
+var b=document.getElementsByTagName("script")[0];
+a.src=document.location.protocol+"//dnn506yrbagrg.cloudfront.net/pages/scripts/0015/0407.js?"+Math.floor(new Date().getTime()/3600000);
+a.async=true;a.type="text/javascript";b.parentNode.insertBefore(a,b)}, 1);
+
