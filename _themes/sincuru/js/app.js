b132552e5a3fc768146e5ccfe089bd412a18f5b1
Mario Vassilides <mario@serieseight.com>
2014-08-15 14:58:19 +0100

    fixed something

--- _themes/sincuru/js/app.js
+++ _themes/sincuru/js/app.js
@@ -1,6 +1,6 @@
 $(document).foundation();
 
-
+/*Smooth Scroll*/
 $(function() {
   $('a[href*=#]:not([href=#])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
@@ -17,7 +17,7 @@ $(function() {
 });
 
 
-   
+/*Chat System*/   
 window.$zopim||(function(d,s){var z=$zopim=function(c){z._.push(c)},$=z.s=
 d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
 _.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute('charset','utf-8');
@@ -25,7 +25,7 @@ $.src='//v2.zopim.com/?2LBYaeysctUSMEaEB0DECpUYYvYmCZWQ';z.t=+new Date;$.
 type='text/javascript';e.parentNode.insertBefore($,e)})(document,'script');
 
 
-
+/*Crazy Egg Analytics System*/
 setTimeout(function(){var a=document.createElement("script");
 var b=document.getElementsByTagName("script")[0];
 a.src=document.location.protocol+"//dnn506yrbagrg.cloudfront.net/pages/scripts/0015/0407.js?"+Math.floor(new Date().getTime()/3600000);
