d1d1bb5172952e7ed4facdd0f76fa45e3d704df1
Mario Vassilides <mario@serieseight.com>
2014-09-10 22:30:33 +0100

    add

--- _themes/sincuru/js/app.js
+++ _themes/sincuru/js/app.js
@@ -31,3 +31,4 @@ var b=document.getElementsByTagName("script")[0];
 a.src=document.location.protocol+"//dnn506yrbagrg.cloudfront.net/pages/scripts/0015/0407.js?"+Math.floor(new Date().getTime()/3600000);
 a.async=true;a.type="text/javascript";b.parentNode.insertBefore(a,b)}, 1);
 
+
